d8f2f53548310322a0ef07bb5c1600ca788d1cec
HRidgl <holly.ridgley1@gmail.com>
2026-07-09 17:55:20 +0100

    check for 3 of the same number in a player hand

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+# Importing the random module so that I can shuffle the deck of cards at the beginning of the game
 import random
 
+# A list of all the decks from the 4 suits in a deck of cards
 deck = ["1S", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS",
         "1C", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC",
         "1D", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD",
@@ -82,9 +84,21 @@ def player_turn(playerNo, players, deck, discard):
             hand.remove(discard)
             break
 
+    check_for_sets(hand)
+
     players[playerNo] = [name, hand]
 
     return deck, players, discard
+
+
+# Distinguish between 1 and 10
+def check_for_sets(hand):
+    for i in range(len(hand)):
+        for j in range(i + 1, len(hand)):
+            for k in range(j + 1, len(hand)):
+                if (hand[i])[0] == (hand[j])[0] == (hand[k])[0]:
+                    print("You have at least 3 of the same number")
+                    print(hand[i], hand[j], hand[k])
 
 
 
